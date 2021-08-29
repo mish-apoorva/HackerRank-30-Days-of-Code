b1a2746d57219d8e463a2dc1c7af9a3074248925
mish-apoorva <apurvamishra.stu@gmail.com>
2021-08-30 02:11:43 +0530

    Day 3

---
+++ 3. Intro to Conditional Statements/Day 3.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input().strip())
+if N%2 !=0 :
+    print("Weird")
+else:
+    if N > 20 :
+        print("Not Weird")
+    elif N >= 6:
+        print("Weird")
+    else :
+        print("Not Weird")
+
+
