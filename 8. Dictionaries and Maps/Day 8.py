9f519720973a09e5f7dc121b5214efabc8596c9f
mish-apoorva <apurvamishra.stu@gmail.com>
2021-09-03 11:14:12 +0530

    Day 8

---
+++ 8. Dictionaries and Maps/Day 8.py
@@ -0,0 +1,15 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n = int(input())
+d = {}
+for i in range(n) :
+    x = input().split()
+    d[x[0]] = x[1]
+while True :
+    try :
+        name = input()
+        if name in d :
+            print(name, '=',d[name], sep = '')
+        else :
+            print('Not found')
+    except: break
