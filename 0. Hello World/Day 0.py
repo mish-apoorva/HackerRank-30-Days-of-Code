47ee712c5ee4304f8f418e3371199b4ae3d4b6e5
mish-apoorva <apurvamishra.stu@gmail.com>
2021-09-05 10:06:34 +0530

    Day 9

--- 0. Hello World/Day 0.py
+++
@@ -1,9 +0,0 @@
-# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
-
-inputString = input()
-
-# Your first line of output goes here
-print('Hello, World.')
-
-# Write the second line of output thatprints the contents of inputString to stdout.
-print(inputString)
