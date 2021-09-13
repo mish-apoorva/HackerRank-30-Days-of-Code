ad7f81e03379f6617787434d254c63f4e2e38f03
mish-apoorva <apurvamishra.stu@gmail.com>
2021-09-13 19:57:33 +0530

    Day 17

---
+++ 17. More Exceptions/Day 17.py
@@ -0,0 +1,28 @@
+#Write your code here
+
+class e(Exception):
+    "n and p should be non-negative"
+
+
+class Calculator():
+    def power(self, n, p):
+        try:
+            if n | p < 0:
+                raise e
+
+            else:
+                return (n ** p)
+        except e:
+            return ("n and p should be non-negative")
+
+
+
+myCalculator=Calculator()
+T=int(input())
+for i in range(T):
+    n,p = map(int, input().split())
+    try:
+        ans=myCalculator.power(n,p)
+        print(ans)
+    except Exception as e:
+        print(e)   
