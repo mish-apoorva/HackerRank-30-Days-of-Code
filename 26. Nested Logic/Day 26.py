91fb88f4a29a14b3d019025c989baebead143fb9
mish-apoorva <apurvamishra.stu@gmail.com>
2021-09-22 20:43:41 +0530

    Day 26

---
+++ 26. Nested Logic/Day 26.py
@@ -0,0 +1,18 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+returned_date = list(map(int,input().split(' ')))
+expected_date = list(map(int,input().split(' ')))
+
+fine = 0
+
+if returned_date[2] > expected_date[2]:
+    fine = 10000
+elif returned_date[2] == expected_date[2]:
+    if returned_date[1] > expected_date[1]:
+        fine = (returned_date[1] - expected_date[1])*500
+    elif returned_date[1] == expected_date[1]:
+        if returned_date[0] > expected_date[0]:
+            fine = (returned_date[0] - expected_date[0])*15
+    
+print(fine)
+
