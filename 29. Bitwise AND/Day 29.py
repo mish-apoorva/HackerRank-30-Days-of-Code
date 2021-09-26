9485b5581e965871b4375cff20686765a7688730
mish-apoorva <apurvamishra.stu@gmail.com>
2021-09-26 20:06:08 +0530

    Day 29

---
+++ 29. Bitwise AND/Day 29.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'bitwiseAnd' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER N
+#  2. INTEGER K
+#
+    # Write your code here
+T = int(input().strip())
+for _ in range(T):
+    n , k = map(int , input().split())
+    print(k-1 if ((k-1) | k) <= n else k-2)
+
+
